b549692a3a0552ac2ec0d8750b6c2fe9aee9a77c
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-25 09:33:34 +0200

    feat: function to get the post's estimated reading time completed

--- app/Functions/Helper.php
+++ app/Functions/Helper.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Str;
 
 class Helper
 {
+    // Generate a unique slug from a given string
     public static function generateSlug($string, $model)
     {
         $original_slug = Str::slug($string, '-');
@@ -13,5 +14,11 @@ class Helper
         $same_original_slug_count = $model::where('slug', 'LIKE', $original_slug . '%')->count();
 
         return $same_original_slug_count ? "{$original_slug}-{$same_original_slug_count}" : $original_slug;
+    }
+
+    // Get the reading time in minutes as an int from the given string
+    public static function getReadingTime($string)
+    {
+        return ceil(str_word_count($string) / 200);
     }
 }
